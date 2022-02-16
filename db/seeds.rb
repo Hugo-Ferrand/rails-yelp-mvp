f647bf03d1df14924dcfd2d2ab5331a5d8339ebf
Hugo Ferrand <hugo.ferrand98@audencia.com>
2022-02-16 15:12:58 +0100

    Table restaurant and Review done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: 'theB', category: 'belgian')
+Restaurant.create(name: 'Parmisano', category: 'italian')
+Restaurant.create(name: 'Dlices', category: 'french')
+Restaurant.create(name: 'DiDong', category: 'chinese')
+Restaurant.create(name: 'NIao', category: 'japanese')
